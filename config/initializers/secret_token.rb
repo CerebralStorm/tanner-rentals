0468d1907507d97a5095548b4132f36dab59928a
Cody Tanner <ctanner@instructure.com>
2014-08-21 22:50:00 -0600

    inital app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+TannerRentals::Application.config.secret_key_base = '9fcbdf0f1bcb60d4a3d844ab7f6a79e28a54fbb5044f2d38f78d9927affacd688b8be1c17ac1d1368e881a6c01c1dedd041dad603a031bb381b24bebf717c2d3'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+TannerRentals::Application.config.session_store :cookie_store, key: '_tanner-rentals_session'
